9dd18e2f293d0214b5968e9d4c860fdb63b6be0b
phsd <paulohenrique@live.com.ph>
2019-07-10 23:57:53 -0300

    primeiro commit

--- telaPrincipal/admin.py
+++ telaPrincipal/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import headerPlanoControle
+from .models import headerPlanoControle, itemInspecaoPC
 
 # Register your models here.
 
 admin.site.register(headerPlanoControle)
+admin.site.register(itemInspecaoPC)
