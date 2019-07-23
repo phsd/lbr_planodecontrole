e7b020cd0e838b3c1064596553ea99c3f248a148
phsd <paulohenrique@live.com.ph>
2019-07-22 23:16:14 -0300

    primeiro commit

--- planocontrole/urls.py
+++ planocontrole/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('telaPrincipal.urls', namespace='telaPrincipal')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
