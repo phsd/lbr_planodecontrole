618a199056986acfd6251ee47e43eed03aeee124
phsd <paulohenrique@live.com.ph>
2019-07-22 23:22:38 -0300

    primeiro commit

--- planocontrole/settings.py
+++ planocontrole/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'telaPrincipal/media')
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
