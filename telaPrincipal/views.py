17bce8a09702f5732cacf5f894c7c91216054c5e
phsd <paulohenrique@live.com.ph>
2019-07-22 23:33:52 -0300

    primeiro commit

--- telaPrincipal/views.py
+++ telaPrincipal/views.py
@@ -105,6 +105,7 @@ def PDF_IPXX_XXX_19(request):
     if os.path.exists(dirname):
         with open(os.path.join(dirname, 'media/pdfs/mypdf2.pdf'), 'wb') as f:
             f.write(pdf_criado)
+            print(dirname + '/media/pdfs/mypdf2.pdf')
             response = FileResponse(open(dirname + '/media/pdfs/mypdf2.pdf', 'rb'), content_type='application/pdf')
             response['Content-Disposition'] = 'inline; filename={title}.pdf'.format(
                 title="titulo")
